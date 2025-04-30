df869dba6b3e657c0e75827d77ae3f11235f7929
James Cacapit <jamescacapit@gmail.com>
2025-04-29 21:45:48 -0700

    no secrets

--- webscraper/main.py
+++ webscraper/main.py
@@ -1,18 +1,22 @@
-
-from Cheaper_Scraper import CheaperScraper
+from api.EbayAPI import EbayAPI
+from src.Cheaper_Scraper import CheaperScraper
 import json
 
 def main():
-    # Set up the scraper for a simple legal-to-scrape website
-    scraper = CheaperScraper("https://books.toscrape.com",
-                              user_agent="CheaperBot/0.1",
-                              delay=2.0)
 
+
+
+    EbayAPI.retrieve_ebay_response("https://api.sandbox.ebay.com/commerce/taxonomy/v1/category_tree/0",'batteries')
+
+    # Set up the scraper for a simple legal-to-scrape website
+   # scraper = CheaperScraper("https://books.toscrape.com",
+    #                          user_agent="CheaperBot/0.1",
+     #                         delay=2.0)
     # Define which pages you want to scrape (you can use "/" for homepage)
     pages = ["/"]
 
     # Use the scraper to fetch and parse the pages
-    results = scraper.scrape(pages)
+    results = CheaperScraper.scraper.scrape(pages)
 
     # Show the output in the terminal
     for path, items in results.items():
