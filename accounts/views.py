fbe28031d22bbfb1617ced720991f4a1620ab87e
JSP24SCM27V <jvijayan1@hawk.iit.edu>
2025-05-05 14:27:49 -0500

    Orms created UserAccount Product models with prod list view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    data = [{"name": p.product_name, "price": float(p.price), "url": p.url} for p in products]
+    return JsonResponse(data, safe=False)
 
 # Create your views here.
