9027a2379db4e6f16736031ca9758ede00339462
jeanahan <jeanahan04@gmail.com>
2025-07-25 09:34:27 -0700

    Wrapped init function with a try/finally

--- webscraper/api/BestBuyApi.py
+++ webscraper/api/BestBuyApi.py
@@ -7,12 +7,13 @@ load_dotenv()
 
 class BestBuyAPI(EbayABC):
     def __init__(self):
-        self.api_key = os.getenv("BESTBUY_API_KEY")
-        self.base_url = "https://api.bestbuy.com/v1/products"
-
-        if not self.api_key:
-            raise ValueError("BESTBUY_API_KEY not found. Check your .env file.")
-
+        try: 
+            self.api_key = os.getenv("BESTBUY_API_KEY")
+            self.base_url = "https://api.bestbuy.com/v1/products"
+        finally: 
+            if not self.api_key:
+                raise Exception ("BESTBUY_API_KEY not found.")
+                
     def retrieve_access_token(self) -> str:
         return self.api_key
 
